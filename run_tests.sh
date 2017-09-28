1fd7484da6ac46d4e8438f12b842f3769afc92b5
Mateusz Stadnik <matgla@live.com>
2017-09-28 21:07:10 +0200

    modified runner script

--- run_tests.sh
+++ run_tests.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
 git submodule update --init
-if [ $RESULT -neq 0]; then
+if [ $? -neq 0]; then
     return -1
 fi
 
 mkdir -p bin_test
-if [ $RESULT -neq 0]; then
+if [ $? -neq 0]; then
     return -1
 fi
 
 cd bin_test
-if [ $RESULT -neq 0]; then
+if [ $? -neq 0]; then
     return -1
 fi
 
 cmake .. -DBUILD_TESTS=ON -DARCH=X86 -GNinja
-if [ $RESULT -neq 0]; then
+if [ $? -neq 0]; then
     return -1
 fi
 
 ninja
-if [ $RESULT -neq 0]; then
+if [ $? -neq 0]; then
     return -1
 fi
 
