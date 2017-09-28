002ff3ad9c9cf87370d3afc0c00614551612ced7
Mateusz Stadnik <matgla@live.com>
2017-09-28 22:48:19 +0200

    added build target

--- build_esp.sh
+++ build_esp.sh
@@ -5,17 +5,17 @@ if [ $? -ne 0 ]; then
     return -1
 fi
 
-cd bin_x86
+cd bin_esp
 if [ $? -ne 0 ]; then
     return -1
 fi
 
-cmake .. -DBUILD_TARGET=ON -DARCH=ESP8266 -GNinja
+source ~/sdk_esp.env && cmake .. -DBUILD_TARGET=ON -DARCH=ESP8266 -GNinja
 if [ $? -ne 0 ]; then
     return -1
 fi
 
-ninja
+source ~/sdk_esp.env && ninja
 if [ $? -ne 0 ]; then
     return -1
 fi
