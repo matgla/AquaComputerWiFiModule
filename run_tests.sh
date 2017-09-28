718bbc13e1378f753d74f22b5d697d1dcd8439c4
Mateusz Stadnik <matgla@live.com>
2017-09-28 21:22:16 +0200

    fixed run test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
 git submodule update --init
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
     return -1
 fi
 
 mkdir -p bin_test
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
     return -1
 fi
 
 cd bin_test
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
     return -1
 fi
 
 cmake .. -DBUILD_TESTS=ON -DARCH=X86 -GNinja
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
     return -1
 fi
 
 ninja
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
     return -1
 fi
 
