5bda7d7b64d77d60b89c357fd82ebf891cb47a64
Mateusz Stadnik <matgla@live.com>
2017-09-28 21:13:54 +0200

    wip build on ci

--- run_tests.sh
+++ run_tests.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
 git submodule update --init
-if [ $? -neq 0]; then
+if [ $? -ne 0]; then
     return -1
 fi
 
 mkdir -p bin_test
-if [ $? -neq 0]; then
+if [ $? -ne 0]; then
     return -1
 fi
 
 cd bin_test
-if [ $? -neq 0]; then
+if [ $? -ne 0]; then
     return -1
 fi
 
 cmake .. -DBUILD_TESTS=ON -DARCH=X86 -GNinja
-if [ $? -neq 0]; then
+if [ $? -ne 0]; then
     return -1
 fi
 
 ninja
-if [ $? -neq 0]; then
+if [ $? -ne 0]; then
     return -1
 fi
 
